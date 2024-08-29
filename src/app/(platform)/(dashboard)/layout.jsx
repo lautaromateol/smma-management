a67f0ff10874fcef06a467cc21f486db7eebf226
lautaromateol <lautaromateol@gmail.com>
2024-08-29 17:10:05 -0300

    Interfaz panel de la aplicación

---
+++ src/app/(platform)/(dashboard)/layout.jsx
@@ -0,0 +1,20 @@
+import { ModalProvider } from "@/providers/modal-provider";
+import { Header } from "./_components/header";
+import { Sidebar } from "./_components/sidebar";
+import { Toaster } from "sonner";
+
+export default function DashboardLayout({ children }) {
+  return (
+    <div className="flex w-full min-h-screen">
+      <Sidebar />
+      <div className="flex w-full flex-col">
+        <Header />
+        <main className="flex-1 p-6">
+          <Toaster />
+          <ModalProvider />
+          {children}
+        </main>
+      </div>
+    </div>
+  )
+}
