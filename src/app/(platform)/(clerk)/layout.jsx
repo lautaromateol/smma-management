1ac7db1b0ebcf983707ddf2a7a35fbd8327e8f94
lautaromateol <lautaromateol@gmail.com>
2024-08-29 17:08:51 -0300

    Interfaz de Clerk

--- src/app/(platform)/(clerk)/layout.jsx
+++ src/app/(platform)/(clerk)/layout.jsx
@@ -1,11 +1,7 @@
-import { ClerkProvider } from "@clerk/nextjs";
-
 export default function ClerkLayout({ children }) {
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <ClerkProvider>
         {children}
-      </ClerkProvider>
     </main>
   )
 }
