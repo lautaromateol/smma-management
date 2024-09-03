1486667db2946ecc72025bdc4320b2b6bc7b26ec
lautaromateol <lautaromateol@gmail.com>
2024-09-03 19:50:09 -0300

    Archivo de titulo y subtitulo relocalizado

--- src/app/(platform)/(dashboard)/agency/[agencyId]/_components/heading.jsx
+++
@@ -1,8 +0,0 @@
-export function Heading({ title, subtitle }) {
-  return (
-    <div className="flex flex-col space-y-1">
-      <h2 className="text-3xl text-main-tint font-semibold">{title}</h2>
-      <span className="text-base font-light text-main">{subtitle}</span>
-    </div>
-  )
-}
