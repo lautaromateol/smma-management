787f47fdd3e2e78554f1fa6e2758127f0a76650b
lautaromateol <lautaromateol@gmail.com>
2024-09-03 17:11:09 -0300

    Logo añadido a header

--- src/app/(platform)/(dashboard)/_components/header.jsx
+++ src/app/(platform)/(dashboard)/_components/header.jsx
@@ -1,14 +1,16 @@
 import { OrganizationSwitcher } from "@clerk/nextjs"
-import { shadesOfPurple, experimental__simple } from "@clerk/themes"
+import { experimental__simple } from "@clerk/themes"
+import { Logo } from "./logo"
 
 export function Header() {
   return (
-    <header className="h-14 shadow p-4 flex items-center justify-end border-b border-main-shade">
-        <OrganizationSwitcher
-          appearance={{
-            baseTheme: experimental__simple
-          }}
-        />
+    <header className="h-14 z-50 sticky top-0 shadow p-4 flex items-center justify-between border-b bg-main-light border-main-shade">
+      <Logo />
+      <OrganizationSwitcher
+        appearance={{
+          baseTheme: experimental__simple
+        }}
+      />
     </header>
   )
 }
