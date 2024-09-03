245b0822f7e6ccedd389ae9b8f8d0faeaf2912f2
lautaromateol <lautaromateol@gmail.com>
2024-09-03 17:21:20 -0300

    Proveedor react-query

---
+++ src/providers/query-provider.jsx
@@ -0,0 +1,13 @@
+"use client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export function QueryProvider({children}) {
+
+  const client = new QueryClient()
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+    </QueryClientProvider>
+  )
+}
