632f69177bb5235407042d0074bd27a023718838
lautaromateol <lautaromateol@gmail.com>
2024-09-03 19:50:56 -0300

    Botones de pagina de cliente actualizados

--- src/app/(platform)/(dashboard)/agency/[agencyId]/clients/_components/clients-buttons.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/clients/_components/clients-buttons.jsx
@@ -4,31 +4,26 @@ import { Button } from "@/components/ui/button";
 import { useOpenModal } from "@/hooks/use-open-modal";
 import { ClientsForm } from "./clients-form";
 
-export default function ClientsButtons() {
+export function ClientsButtons() {
 
   const { onOpen } = useOpenModal((state) => state)
 
   return (
-    <div className="flex items-center gap-x-4">
-      <div>
-        Search
-      </div>
-      <div>
-        Filter
-      </div>
+    <>
       <Button
-        onClick={onOpen}
+        onClick={() => onOpen("add-client-form")}
         variant="main"
         size="sm"
       >
         Add new client
       </Button>
       <Modal
+        modalId="add-client-form"
         title="Add new client"
         description="Fill out the form fields to add a new client to your database"
       >
         <ClientsForm />
       </Modal>
-    </div>
+    </>
   )
 }
