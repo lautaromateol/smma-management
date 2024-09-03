9da4c3bd16ff444d33f73a4145988f41b83ffde8
lautaromateol <lautaromateol@gmail.com>
2024-09-03 17:20:34 -0300

    Función para formatear fecha creada

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(date) {
+  const endDate = new Date(date)
+  const day = endDate.getDate()
+  const month = endDate.getMonth() + 1
+  const year = endDate.getFullYear()
+  const formatedDay = day.toString().padStart(2, "0")
+  const formatedMonth = month.toString().padStart(2, "0")
+
+  return`${formatedDay}/${formatedMonth}/${year}`
+
+}
