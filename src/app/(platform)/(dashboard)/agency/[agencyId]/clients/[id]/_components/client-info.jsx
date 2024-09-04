044fd2dd7754e0dbffceb1410c0dae866568df08
lautaromateol <lautaromateol@gmail.com>
2024-09-04 12:20:07 -0300

    Componente de información de cliente

---
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/clients/[id]/_components/client-info.jsx
@@ -0,0 +1,22 @@
+export function ClientInfo({ client }) {
+  return (
+    <div className="flex items-center gap-x-6">
+      <div className="flex items-center gap-x-2">
+        <p className="text-sm font-light">Phone:</p>
+        <p className="text-sm">{client.phone}</p>
+      </div>
+      <div className="flex items-center gap-x-2">
+        <p className="text-sm font-light">Company:</p>
+        <p className="text-sm">{client.company}</p>
+      </div>
+      <div className="flex items-center gap-x-2">
+        <p className="text-sm font-light">Industry:</p>
+        <p className="text-sm">{client.industry}</p>
+      </div>
+      <div className="flex items-center gap-x-2">
+        <p className="text-sm font-light">Website:</p>
+        <a target="_blank" href={client.website} className="text-sm hover:underline">{client.website}</a>
+      </div>
+    </div>
+  )
+}
