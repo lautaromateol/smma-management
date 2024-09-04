ea6f92cbc113c398be34d3e5c61ea3b75a8c9a7a
lautaromateol <lautaromateol@gmail.com>
2024-09-04 12:21:31 -0300

    Corrección en validación de fechas

--- src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/_components/campaigns-form.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/_components/campaigns-form.jsx
@@ -235,7 +235,7 @@ export function CampaignsForm({ editValues = {}, clients }) {
                     mode="single"
                     selected={field.value}
                     onSelect={field.onChange}
-                    disabled={(date) => form.getValues().startDate ? date < new Date(form.getValues().startDate) : date < new Date()}
+                    disabled={(date) => form.getValues().start ? date < new Date(form.getValues().start) : date < new Date()}
                     initialFocus
                   />
                 </PopoverContent>
