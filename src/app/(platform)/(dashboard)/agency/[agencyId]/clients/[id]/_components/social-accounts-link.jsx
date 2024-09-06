ee324be6023d9128fa3a663d83b9f7b6a604afc7
lautaromateol <lautaromateol@gmail.com>
2024-09-06 11:55:44 -0300

    Botones de vinculación a redes sociales

---
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/clients/[id]/_components/social-accounts-link.jsx
@@ -0,0 +1,54 @@
+import { Instagram, Linkedin, Twitter } from "lucide-react";
+import { CardWrapper } from "../../../_components";
+import { Button } from "@/components/ui/button";
+import { FacebookButton } from ".";
+
+export function SocialAccountsLink({ id }) {
+  return (
+    <CardWrapper
+      title="Social media accounts"
+      description="Link your client's social media accounts to start working on your campaigns"
+      className="border-none"
+    >
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+       <FacebookButton id={id} />
+        <div className="flex items-center justify-between border p-4 rounded-md">
+          <div className="flex items-center gap-x-2">
+            <Instagram className="size-4" />
+            <p className="text-sm font-medium">Instagram</p>
+          </div>
+          <Button
+            variant="outline"
+            size="sm"
+          >
+            Link
+          </Button>
+        </div>
+        <div className="flex items-center justify-between border p-4 rounded-md">
+          <div className="flex items-center gap-x-2">
+            <Twitter className="size-4" />
+            <p className="text-sm font-medium">X</p>
+          </div>
+          <Button
+            variant="outline"
+            size="sm"
+          >
+            Link
+          </Button>
+        </div>
+        <div className="flex items-center justify-between border p-4 rounded-md">
+          <div className="flex items-center gap-x-2">
+            <Linkedin className="size-4" />
+            <p className="text-sm font-medium">LinkedIn</p>
+          </div>
+          <Button
+            variant="outline"
+            size="sm"
+          >
+            Link
+          </Button>
+        </div>
+      </div>
+    </CardWrapper>
+  )
+}
