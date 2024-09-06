34a041a77653b6f0a411420679d59478a3fb7ec0
lautaromateol <lautaromateol@gmail.com>
2024-09-06 12:09:06 -0300

    Redirect URI de facebook actualizada

--- src/constants/facebook.js
+++ src/constants/facebook.js
@@ -1,2 +1,2 @@
 export const FACEBOOK_CLIENT_ID="3584568831798501"
-export const FACEBOOK_REDIRECT_URI="https://e370-181-98-157-239.ngrok-free.app/api/facebook-auth"
+export const FACEBOOK_REDIRECT_URI="https://adsync.vercel.app/api/facebook-auth"
