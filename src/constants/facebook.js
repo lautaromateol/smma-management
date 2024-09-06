d48e3547567d3b0546c24c7797affd55471ef593
lautaromateol <lautaromateol@gmail.com>
2024-09-06 11:56:42 -0300

    Constantes de facebook actualizadas

--- src/constants/facebook.js
+++ src/constants/facebook.js
@@ -1,2 +1,2 @@
 export const FACEBOOK_CLIENT_ID="3584568831798501"
-export const FACEBOOK_REDIRECT_URI="https://f053-181-4-57-248.ngrok-free.app/api/facebook-auth"
+export const FACEBOOK_REDIRECT_URI="https://e370-181-98-157-239.ngrok-free.app/api/facebook-auth"
