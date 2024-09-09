35abfe4bee9622e4d514a16ca97d1837f251e337
lautaromateol <lautaromateol@gmail.com>
2024-09-09 14:36:18 -0300

    Botones de facebook e instagram unificados

---
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/clients/[id]/_components/meta-button.jsx
@@ -0,0 +1,68 @@
+"use client"
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { FACEBOOK_CLIENT_ID, FACEBOOK_REDIRECT_URI } from "@/constants/facebook";
+import { fetcher } from "@/lib/fetcher";
+import { useQuery } from "@tanstack/react-query";
+import { FaMeta, FaFacebook } from "react-icons/fa6";
+import { isAfter } from "date-fns";
+
+export function MetaButton({ client }) {
+
+  function onClick() {
+    window.location.href = `https://www.facebook.com/v20.0/dialog/oauth?client_id=${FACEBOOK_CLIENT_ID}&redirect_uri=${FACEBOOK_REDIRECT_URI}&state="{id=${client.id},agencyId=${client.orgId}}"&scope=instagram_basic,instagram_content_publish,instagram_manage_insights,pages_manage_posts,pages_read_engagement,ads_management,ads_read,business_management,public_profile`
+  }
+
+  const { data, isPending } = useQuery({
+    queryKey: ["client-meta-data", client.id],
+    queryFn: () => fetcher(`/api/clients/meta-data?token=${client.metaAccessToken.token}`)
+  })
+
+  const isTokenExpired = client.metaAccessToken ? isAfter(new Date(), new Date(client.metaAccessToken.expiresIn)) : false
+
+  if (isPending && client.metaAccessToken) {
+    return (
+      <Skeleton className="h-20 col-span-2" />
+    )
+  }
+
+  if (!client.metaAccessToken || isTokenExpired) {
+    return (
+      <div className="flex flex-col gap-y-2 col-span-2">
+        {isTokenExpired && 
+          <div className="bg-red-200 text-destructive border border-destructive p-2 font-medium text-sm rounded-md">
+            The Meta access token for this account is expired. Please authenticate again.
+          </div>
+        }
+        <div className="flex items-center justify-between border p-4 rounded-md">
+          <div className="flex items-center gap-x-2">
+            <FaMeta className="size-4" />
+            <p className="text-sm font-medium">Meta</p>
+          </div>
+          <Button
+            onClick={onClick}
+            variant="outline"
+            size="sm"
+          >
+            Link
+          </Button>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex items-center gap-x-2 border p-4 rounded-md col-span-2">
+      <Image
+        src={data.picture.data.url}
+        width={40}
+        height={40}
+        className="rounded-full"
+        alt="User Meta profile picture"
+      />
+      <p className="font-medium text-base text-neutral-700">{`${data.first_name} ${data.last_name}`}</p>
+      <FaFacebook className="size-4" />
+    </div>
+  )
+}
--- src/app/(platform)/(dashboard)/agency/[agencyId]/clients/[id]/_components/social-accounts-link.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/clients/[id]/_components/social-accounts-link.jsx
@@ -1,29 +1,18 @@
-import { Instagram, Linkedin, Twitter } from "lucide-react";
+import { Linkedin, Twitter } from "lucide-react";
 import { CardWrapper } from "../../../_components";
 import { Button } from "@/components/ui/button";
-import { FacebookButton } from ".";
+import { MetaButton } from ".";
 
-export function SocialAccountsLink({ id, orgId }) {
+export async function SocialAccountsLink({ client }) {
+
   return (
     <CardWrapper
       title="Social media accounts"
       description="Link your client's social media accounts to start working on your campaigns"
       className="border-none"
     >
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-       <FacebookButton id={id} orgId={orgId} />
-        <div className="flex items-center justify-between border p-4 rounded-md">
-          <div className="flex items-center gap-x-2">
-            <Instagram className="size-4" />
-            <p className="text-sm font-medium">Instagram</p>
-          </div>
-          <Button
-            variant="outline"
-            size="sm"
-          >
-            Link
-          </Button>
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 w-1/2">
+       <MetaButton client={client} />
         <div className="flex items-center justify-between border p-4 rounded-md">
           <div className="flex items-center gap-x-2">
             <Twitter className="size-4" />
