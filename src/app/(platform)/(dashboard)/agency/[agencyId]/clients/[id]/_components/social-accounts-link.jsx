7f46db12ac65cd965d76ad9d3f5714ef69f2b9ae
lautaromateol <lautaromateol@gmail.com>
2024-09-11 17:49:14 -0300

    Botones de redes sociales componentizados

--- src/app/(platform)/(dashboard)/agency/[agencyId]/clients/[id]/_components/social-accounts-link.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/clients/[id]/_components/social-accounts-link.jsx
@@ -1,7 +1,5 @@
-import { Linkedin, Twitter } from "lucide-react";
 import { CardWrapper } from "../../../_components";
-import { Button } from "@/components/ui/button";
-import { MetaButton } from ".";
+import { LinkedInButton, MetaButton, TwitterButton } from ".";
 
 export async function SocialAccountsLink({ client }) {
 
@@ -11,32 +9,10 @@ export async function SocialAccountsLink({ client }) {
       description="Link your client's social media accounts to start working on your campaigns"
       className="border-none"
     >
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 w-1/2">
-       <MetaButton client={client} />
-        <div className="flex items-center justify-between border p-4 rounded-md">
-          <div className="flex items-center gap-x-2">
-            <Twitter className="size-4" />
-            <p className="text-sm font-medium">X</p>
-          </div>
-          <Button
-            variant="outline"
-            size="sm"
-          >
-            Link
-          </Button>
-        </div>
-        <div className="flex items-center justify-between border p-4 rounded-md">
-          <div className="flex items-center gap-x-2">
-            <Linkedin className="size-4" />
-            <p className="text-sm font-medium">LinkedIn</p>
-          </div>
-          <Button
-            variant="outline"
-            size="sm"
-          >
-            Link
-          </Button>
-        </div>
+      <div className="space-y-4">
+        <MetaButton client={client} />
+        <TwitterButton client={client} />
+        <LinkedInButton client={client} />
       </div>
     </CardWrapper>
   )
