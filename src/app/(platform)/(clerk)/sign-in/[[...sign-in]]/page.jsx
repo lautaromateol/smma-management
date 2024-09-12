2dbb57bc08592978570a78b987a1826fce70c426
lautaromateol <lautaromateol@gmail.com>
2024-09-11 23:37:15 -0300

    Paginas de autenticación re-estilizadas

--- src/app/(platform)/(clerk)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(platform)/(clerk)/sign-in/[[...sign-in]]/page.jsx
@@ -1,7 +1,30 @@
+import { Logo } from "@/components/logo";
 import { SignIn } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function SignInPage() {
   return (
-    <SignIn />
+    <div className="flex flex-col items-center rounded-md shadow-md px-2 py-6 min-h-56">
+      <Logo />
+      <SignIn appearance={{
+        elements: {
+          card: {
+            paddingTop: "8px",
+            boxShadow: "none",
+            border: "none"
+          },
+          cardBox: {
+            boxShadow: "none",
+            border: "none"
+          },
+          footer: {
+            display: "none"
+          }
+        }
+      }} />
+      <Link href="/sign-up" className="text-sm text-muted-foreground underline">
+        Don&apos;t you have an account? Sign up now
+      </Link>
+    </div>
   )
 }
--- src/app/(platform)/(clerk)/sign-up/[[...sign-up]]/page.jsx
+++ src/app/(platform)/(clerk)/sign-up/[[...sign-up]]/page.jsx
@@ -1,7 +1,30 @@
+import { Logo } from "@/components/logo";
 import { SignUp } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function SignUpPage() {
   return (
-    <SignUp />
+    <div className="flex flex-col items-center rounded-md shadow-md px-2 py-6 min-h-56">
+    <Logo />
+    <SignUp appearance={{
+      elements: {
+        card: {
+          paddingTop: "8px",
+          boxShadow: "none",
+          border: "none"
+        },
+        cardBox: {
+          boxShadow: "none",
+          border: "none"
+        },
+        footer: {
+          display: "none"
+        }
+      }
+    }} />
+    <Link href="/sign-in" className="text-sm text-muted-foreground underline">
+    Already registered? Sign in now
+    </Link>
+  </div>
   )
 }
