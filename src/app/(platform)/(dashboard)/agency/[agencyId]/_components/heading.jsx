a9e008372b8ddec9ac06bdcc6be17fd7da3d2e5a
lautaromateol <lautaromateol@gmail.com>
2024-09-12 18:24:54 -0300

    Diseño responsivo para heading

--- src/app/(platform)/(dashboard)/agency/[agencyId]/_components/heading.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/_components/heading.jsx
@@ -1,8 +1,8 @@
 export function Heading({ title, subtitle }) {
   return (
-    <div className="flex flex-col space-y-1">
-      <h2 className="text-3xl text-main-tint font-semibold">{title}</h2>
-      <span className="text-base font-light text-main">{subtitle}</span>
+    <div className="flex flex-col md:gap-y-1">
+      <h2 className="text-center md:text-left text-2xl md:text-3xl text-main-tint font-semibold">{title}</h2>
+      <p className="text-center md:text-left text-sm md:text-base font-light text-main">{subtitle}</p>
     </div>
   )
 }
