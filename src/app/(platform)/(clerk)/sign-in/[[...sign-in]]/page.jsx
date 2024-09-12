1dc1aa33ed2cfac2a12ab362aeb88f35cacdf4e5
lautaromateol <lautaromateol@gmail.com>
2024-09-11 23:47:29 -0300

    Estado de carga añadido a paginas de autenticación

--- src/app/(platform)/(clerk)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(platform)/(clerk)/sign-in/[[...sign-in]]/page.jsx
@@ -1,30 +1,38 @@
 import { Logo } from "@/components/logo";
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, useSignIn } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function SignInPage() {
-  return (
-    <div className="flex flex-col items-center rounded-md shadow-md px-2 py-6 min-h-56">
-      <Logo />
-      <SignIn appearance={{
-        elements: {
-          card: {
-            paddingTop: "8px",
-            boxShadow: "none",
-            border: "none"
-          },
-          cardBox: {
-            boxShadow: "none",
-            border: "none"
-          },
-          footer: {
-            display: "none"
+
+  const { isLoaded } = useSignIn()
+
+  if(isLoaded) {
+    return (
+      <div className="flex flex-col items-center rounded-md shadow-md px-2 py-6">
+        <Logo />
+        <SignIn appearance={{
+          elements: {
+            card: {
+              paddingTop: "8px",
+              boxShadow: "none",
+              border: "none"
+            },
+            cardBox: {
+              boxShadow: "none",
+              border: "none"
+            },
+            footer: {
+              display: "none"
+            }
           }
-        }
-      }} />
-      <Link href="/sign-up" className="text-sm text-muted-foreground underline">
-        Don&apos;t you have an account? Sign up now
-      </Link>
-    </div>
-  )
+        }} />
+        <Link href="/sign-up" className="text-sm text-muted-foreground underline">
+          Don&apos;t you have an account? Sign up now
+        </Link>
+      </div>
+    )
+  }
+
+  return null
+
 }
--- src/app/(platform)/(clerk)/sign-up/[[...sign-up]]/page.jsx
+++ src/app/(platform)/(clerk)/sign-up/[[...sign-up]]/page.jsx
@@ -1,30 +1,37 @@
 import { Logo } from "@/components/logo";
-import { SignUp } from "@clerk/nextjs";
+import { SignUp, useSignUp } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function SignUpPage() {
-  return (
-    <div className="flex flex-col items-center rounded-md shadow-md px-2 py-6 min-h-56">
-    <Logo />
-    <SignUp appearance={{
-      elements: {
-        card: {
-          paddingTop: "8px",
-          boxShadow: "none",
-          border: "none"
-        },
-        cardBox: {
-          boxShadow: "none",
-          border: "none"
-        },
-        footer: {
-          display: "none"
+
+  const { isLoaded } = useSignUp()
+
+  if(isLoaded) {
+    return (
+      <div className="flex flex-col items-center rounded-md shadow-md px-2 py-6 min-h-56">
+      <Logo />
+      <SignUp appearance={{
+        elements: {
+          card: {
+            paddingTop: "8px",
+            boxShadow: "none",
+            border: "none"
+          },
+          cardBox: {
+            boxShadow: "none",
+            border: "none"
+          },
+          footer: {
+            display: "none"
+          }
         }
-      }
-    }} />
-    <Link href="/sign-in" className="text-sm text-muted-foreground underline">
-    Already registered? Sign in now
-    </Link>
-  </div>
-  )
+      }} />
+      <Link href="/sign-in" className="text-sm text-muted-foreground underline">
+      Already registered? Sign in now
+      </Link>
+    </div>
+    )
+  }
+
+  return null
 }
