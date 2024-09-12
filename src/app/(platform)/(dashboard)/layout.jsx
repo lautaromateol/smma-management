83a3343e6fe8efb6548257abd04c3f1ed017a526
lautaromateol <lautaromateol@gmail.com>
2024-09-12 18:24:26 -0300

    Botón para mostrar sidebar agregado a interfaz

--- src/app/(platform)/(dashboard)/layout.jsx
+++ src/app/(platform)/(dashboard)/layout.jsx
@@ -1,11 +1,16 @@
+import { auth } from "@clerk/nextjs/server";
 import { Header } from "./_components/header";
 import { Sidebar } from "./_components/sidebar";
 import { Toaster } from "sonner";
+import { HandleSidebarBtn } from "./_components/hadle-sidebar-button";
 
 export default function DashboardLayout({ children }) {
+
+  const { orgId } = auth()
+
   return (
-    <div className="flex w-full min-h-screen">
-      <Sidebar />
+    <div className="relative flex w-full min-h-screen">
+      <Sidebar orgId={orgId} />
       <div className="flex w-full flex-col">
         <Header />
         <main className="flex-1 p-6">
@@ -13,6 +18,7 @@ export default function DashboardLayout({ children }) {
           {children}
         </main>
       </div>
+      <HandleSidebarBtn />
     </div>
   )
 }
