c794ed8e5d5a538c15ddd6436d4980bcc3f29d7a
lautaromateol <lautaromateol@gmail.com>
2024-09-11 23:47:11 -0300

    UI de carga añadida

---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-main-light">
+      <div className="h-16 w-16 animate-spin rounded-full border-2 border-solid border-main-tint border-t-transparent" />
+    </div>
+  )
+}
