620358bbb343e6edcd62142f9e2ebdd09eb9eedf
lautaromateol <lautaromateol@gmail.com>
2024-09-16 23:33:30 -0300

    URL de API Graph añadida a constantes

--- src/constants/facebook.js
+++ src/constants/facebook.js
@@ -1,2 +1,3 @@
+export const FACEBOOK_API_GRAPH_URL = "https://graph.facebook.com/v20.0"
 export const FACEBOOK_CLIENT_ID = "3584568831798501"
 export const FACEBOOK_REDIRECT_URI="https://adsync.vercel.app/success/facebook"
