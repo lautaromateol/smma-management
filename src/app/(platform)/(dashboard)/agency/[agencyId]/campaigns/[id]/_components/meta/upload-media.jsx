2b21efeaadb6cf13ef961e38d5275f41777e1fa5
lautaromateol <lautaromateol@gmail.com>
2024-09-16 23:35:46 -0300

    Nuevos componentes para form de publicacion fb

---
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/upload-media.jsx
@@ -0,0 +1,67 @@
+import { MediaPreview } from ".";
+import { toast } from "sonner";
+import { FormLabel } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { FACEBOOK_API_GRAPH_URL } from "@/constants/facebook";
+
+export function UploadMedia({ form, fbPageId, accessToken, mediaFbIds, setMediaFbIds }) {
+
+  async function handleSelectImage(e) {
+    const image = e.target.files[0]
+
+    const formData = new FormData()
+
+    formData.append("source", image)
+    formData.append("access_token", accessToken)
+    formData.append("published", false)
+    if (!form.getValues()?.published) {
+      formData.append("temporary", true)
+    }
+
+    try {
+      const response = await fetch(`${FACEBOOK_API_GRAPH_URL}/${fbPageId}/photos`,
+        {
+          method: "POST",
+          body: formData
+        })
+
+      const data = await response.json()
+
+      if (response.ok) {
+
+        toast.success(data.id)
+
+        setMediaFbIds((curr) => [...curr, { media_fbid: data.id }])
+      } else {
+        toast.error("There was an error uploading the image")
+        console.log(data.error.message)
+      }
+    } catch (error) {
+      toast.error("There was an error uploading the image")
+    }
+  }
+
+  function handleSelectVideo(e) {
+    setSelectedVideo(e.target.files[0])
+  }
+
+  return (
+    <div className="bg-white space-y-2 p-4">
+      <FormLabel>Media</FormLabel>
+      <p className="text-sm">Share photos or a video. Instagram posts can&apos;t exceed 10 photos.</p>
+      <div className="space-y-4">
+        <div className="flex items-center gap-x-2 mt-2">
+          <div className="space-y-1">
+            <p className="text-sm font-medium">Add photo</p>
+            <Input type="file" onChange={handleSelectImage} />
+          </div>
+          <div className="space-y-1">
+            <p className="text-sm font-medium">Add video</p>
+            <Input type="file" onChange={handleSelectVideo} />
+          </div>
+        </div>
+        <MediaPreview mediaFbIds={mediaFbIds} accessToken={accessToken} />
+      </div>
+    </div>
+  )
+}
