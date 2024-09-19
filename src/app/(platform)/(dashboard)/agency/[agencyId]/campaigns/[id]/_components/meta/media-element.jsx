5032963e237c8ce5fec82cab592c3a66db33a754
lautaromateol <lautaromateol@gmail.com>
2024-09-19 16:21:22 -0300

    Funcionalidad para cargar imagen grande en estado

--- src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/media-element.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/media-element.jsx
@@ -1,15 +1,36 @@
-import { Skeleton } from "@/components/ui/skeleton"
-import { FACEBOOK_API_GRAPH_URL } from "@/constants/facebook"
-import { fetcher } from "@/lib/fetcher"
-import { useQuery } from "@tanstack/react-query"
+"use client"
 import Image from "next/image"
+import { useEffect } from "react"
+import { useQuery } from "@tanstack/react-query"
+import { useFormInputs } from "@/hooks/use-form-inputs"
+import { fetcher } from "@/lib/fetcher"
+import { FACEBOOK_API_GRAPH_URL } from "@/constants/facebook"
+import { Skeleton } from "@/components/ui/skeleton"
 
 export function MediaElement({ id, accessToken }) {
 
+  const { inputs, setInputs } = useFormInputs((state) => state)
+
+  const { images } = inputs
+
   const { data, isPending } = useQuery({
     queryKey: ["media-data", id],
-    queryFn: () => fetcher(`${FACEBOOK_API_GRAPH_URL}/${id}?fields=images&access_token=${accessToken}`)
+    queryFn: () => fetcher(`${FACEBOOK_API_GRAPH_URL}/${id}?fields=images&access_token=${accessToken}`),
+
   })
+
+  useEffect(() => {
+    if (data && data?.images) {
+      const image = data.images.find((img) => img.height >= 200 && img.height <= 300);
+
+      const alreadyExists = images.some((img) => img?.source === image?.source);
+
+      if (!alreadyExists && image) {
+        setInputs("images", [...images, image]);
+      }
+    }
+  }, [data, images, setInputs]);
+
 
   if (isPending) {
     return (
