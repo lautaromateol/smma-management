9204a0c7eb43940d9416c9695e2439abb7c43284
lautaromateol <lautaromateol@gmail.com>
2024-09-20 15:18:13 -0300

    Popover agregado a botón para insertar link

--- src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/insert-link.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/insert-link.jsx
@@ -5,11 +5,17 @@ import { FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { LinkIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { useFormInputs } from "@/hooks/use-form-inputs";
+import { FaFacebook } from "react-icons/fa6";
 
 export function InsertLink({ form, message }) {
 
   const [linkValue, setLinkValue] = useState("")
   const [showLinkForm, setShowLinkForm] = useState(false)
+  const [showPopover, setShowPopover] = useState(false)
+
+  const { inputs: { attached_media } } = useFormInputs((state) => state)
 
   function handleCancelLink() {
     setShowLinkForm(false)
@@ -23,14 +29,35 @@ export function InsertLink({ form, message }) {
 
   return (
     <>
-      <Button
-        variant="ghost"
-        type="button"
-        onClick={() => setShowLinkForm((curr) => !curr)}
-        className="grid place-content-center p-4 rounded-full hover:bg-main-light"
-      >
-        <LinkIcon className="size-4" />
-      </Button>
+      <Popover open={showPopover} onOpenChange={setShowPopover}>
+        <PopoverContent>
+          <p className="font-semibold text-sm mb-2">Link preview</p>
+          <p className="text-sm mb-1">
+            {
+              attached_media?.length ?
+                "To preview a link, remove the media that you selected." :
+                "Add a link to preview and use the image from this link in your post."
+            }
+          </p>
+          <div className="flex items-center gap-x-2">
+            <FaFacebook className="size-4" />
+            <p className="text-sm">Facebook only</p>
+          </div>
+        </PopoverContent>
+        <PopoverTrigger asChild>
+          <Button
+            variant="ghost"
+            type="button"
+            disabled={attached_media?.length}
+            onMouseEnter={() => setShowPopover(true)}
+            onMouseLeave={() => setShowPopover(false)}
+            onClick={() => setShowLinkForm((curr) => !curr)}
+            className="grid place-content-center p-4 rounded-full hover:bg-main-light"
+          >
+            <LinkIcon className="size-4" />
+          </Button>
+        </PopoverTrigger>
+      </Popover>
       {showLinkForm && (
         <div className="w-full space-y-2">
           <FormLabel className={cn(message && "text-destructive")}>Link preview</FormLabel>
