52f98a8cf09620b0d4c17d2bf5d8f7994ae5716b
lautaromateol <lautaromateol@gmail.com>
2024-09-26 00:50:34 -0300

    Carrusel de imagenes instagram finalizado

--- src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/images-carousel.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/images-carousel.jsx
@@ -18,24 +18,23 @@ export function ImagesCarousel({ images }) {
 
   return (
     <div className="h-96 relative overflow-hidden">
-      <div
+       <div
         className="flex transition-transform duration-500 ease-in-out"
         style={{ transform: `translateX(-${index * 100}%)` }}
       >
         {images.map((image, i) => (
-          <div key={i} className="w-full h-96 flex-shrink-0">
+          <div key={i} className="w-full h-96 flex-shrink-0 relative">
             <Image
               src={image.source}
-              className="object-cover"
-              alt={`Carrousel image ${i + 1}`}
-              fill
+              layout="fill"
+              objectFit="cover"
+              alt={`Carousel image ${i + 1}`}
             />
           </div>
         ))}
-
       </div>
 
-      <div className="absolute flex items-center justify-center gap-x-0.5 bottom-0 inset-x-0">
+      <div className="absolute flex items-center justify-center gap-x-0.5 bottom-2 inset-x-0">
         {images.map((_, i) => (
           <div className={cn(
             "size-1.5 rounded-full",
@@ -46,14 +45,14 @@ export function ImagesCarousel({ images }) {
       <div
         onClick={handlePrev}
         role="button"
-        className="absolute left-5 top-1/2 transform -translate-y-1/2"
+        className="absolute left-2 top-1/2 transform -translate-y-1/2"
       >
         <ChevronLeft className="font-semibold text-white text-border size-12" />
       </div>
       <div
         onClick={handleNext}
         role="button"
-        className="absolute left-5 top-1/2 transform -translate-y-1/2"
+        className="absolute right-2 top-1/2 transform -translate-y-1/2"
       >
         <ChevronRight className="font-semibold text-white text-border size-12" />
       </div>
