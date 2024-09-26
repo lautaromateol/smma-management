aed517a1fbaf477b119b67601336cae66a1ec659
lautaromateol <lautaromateol@gmail.com>
2024-09-25 23:27:34 -0300

    Clase de tailwind añadida

--- tailwind.config.js
+++ tailwind.config.js
@@ -73,6 +73,9 @@ module.exports = {
           to: { height: "0" },
         },
       },
+      textShadow: {
+        'text-border': '0px 0px 2px black',
+      },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
