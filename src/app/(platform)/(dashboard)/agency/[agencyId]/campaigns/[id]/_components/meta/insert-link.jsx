2239b2f2ca770397a66626695dc591852a7b8a32
lautaromateol <lautaromateol@gmail.com>
2024-10-11 21:18:40 -0300

    Arquitectura componente insert link actualizada

--- src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/insert-link.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/insert-link.jsx
@@ -8,13 +8,35 @@ import { cn } from "@/lib/utils";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { useFormInputs } from "@/hooks/use-inputs";
 
-export function InsertLink({ form, message }) {
+export function InsertLink({ setShowLinkForm }) {
 
-  const [linkValue, setLinkValue] = useState("")
-  const [showLinkForm, setShowLinkForm] = useState(false)
   const [showPopover, setShowPopover] = useState(false)
 
   const { inputs: { attached_media } } = useFormInputs((state) => state)
+  return (
+    <Popover open={showPopover} onOpenChange={setShowPopover}>
+      <PopoverContent>
+        <p className="font-semibold text-sm mb-2">Link preview</p>
+        <p className="text-sm mb-1">Add a link to preview and use the image from this link in your post.</p>
+      </PopoverContent>
+      <PopoverTrigger asChild>
+        <Button
+          variant="ghost"
+          type="button"
+          disabled={attached_media?.length}
+          onMouseEnter={() => setShowPopover(true)}
+          onMouseLeave={() => setShowPopover(false)}
+          onClick={() => setShowLinkForm((curr) => !curr)}
+          className="grid place-content-center p-4 rounded-full hover:bg-main-light"
+        >
+          <LinkIcon className="size-4" />
+        </Button>
+      </PopoverTrigger>
+    </Popover>
+  )
+}
+
+InsertLink.Form = function InserLinkForm({ form, setShowLinkForm, message, linkValue, setLinkValue, showLinkForm }) {
 
   function handleCancelLink() {
     setShowLinkForm(false)
@@ -28,25 +50,6 @@ export function InsertLink({ form, message }) {
 
   return (
     <>
-      <Popover open={showPopover} onOpenChange={setShowPopover}>
-        <PopoverContent>
-          <p className="font-semibold text-sm mb-2">Link preview</p>
-          <p className="text-sm mb-1">Add a link to preview and use the image from this link in your post.</p>
-        </PopoverContent>
-        <PopoverTrigger asChild>
-          <Button
-            variant="ghost"
-            type="button"
-            disabled={attached_media?.length}
-            onMouseEnter={() => setShowPopover(true)}
-            onMouseLeave={() => setShowPopover(false)}
-            onClick={() => setShowLinkForm((curr) => !curr)}
-            className="grid place-content-center p-4 rounded-full hover:bg-main-light"
-          >
-            <LinkIcon className="size-4" />
-          </Button>
-        </PopoverTrigger>
-      </Popover>
       {showLinkForm && (
         <div className="w-full space-y-2">
           <FormLabel className={cn(message && "text-destructive")}>Link preview</FormLabel>
