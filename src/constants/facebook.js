ea35200928373d1e2410f0fe087f98f3fd723bc2
lautaromateol <lautaromateol@gmail.com>
2024-10-17 18:56:32 -0300

    Versión de la API de Meta actualizada

--- src/constants/facebook.js
+++ src/constants/facebook.js
@@ -1,3 +1,3 @@
-export const FACEBOOK_API_GRAPH_URL = "https://graph.facebook.com/v20.0"
+export const FACEBOOK_API_GRAPH_URL = "https://graph.facebook.com/v21.0"
 export const FACEBOOK_CLIENT_ID = "3584568831798501"
 export const FACEBOOK_REDIRECT_URI= process.env.NODE_ENV === "development" ? "https://example.com/" : "https://adsync.vercel.app/success/facebook"  
