99507cb6bdba9051f88a34262c594babc6ade71e
lautaromateol <lautaromateol@gmail.com>
2024-10-17 01:07:03 -0300

    Funcionalidad de subida multiple de historias IG

--- src/actions/publish-instagram-story/index.js
+++ src/actions/publish-instagram-story/index.js
@@ -4,7 +4,6 @@ import { InstagramStory } from "./schema";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { FACEBOOK_API_GRAPH_URL } from "@/constants/facebook";
 import { publishContainerId } from "@/lib/publish-container-id";
-import { isContainerReady } from "@/lib/is-container-ready";
 
 export async function handler(data) {
   const { userId, orgId } = auth()
@@ -17,104 +16,25 @@ export async function handler(data) {
 
   try {
 
-    if (urls.length === 1) {
+      const video = urls.filter((url) => url.type === "video")[0]
 
-      const url = urls[0]
+      const mediaPromises = urls.map((item) => {
 
-      if (url.type === "image") {
         const formData = new FormData()
-
-        formData.append("image_url", url.source)
         formData.append("media_type", "STORIES")
-        formData.append("access_token", access_token)
 
-        const response = await fetch(`${FACEBOOK_API_GRAPH_URL}/${id}/media`, {
-          method: "POST",
-          body: formData
-        })
-
-        const containerData = await response.json()
-
-        if (containerData.id) {
-
-          const upload = await publishContainerId(data, containerData.id)
-
-          if (upload.id) {
-            return { ok: true, id: upload.id }
-          } else {
-            console.log(upload)
-            throw new Error(upload.error.message)
-          }
-
+        if (item.type === "image") {
+          formData.append("image_url", item.source)
         } else {
-          return { error: "Error uploading the story to Instagram!" }
+          formData.append("upload_type", "resumable")
         }
-      } else {
-        const formData = new FormData()
 
-        formData.append("media_type", "STORIES")
-        formData.append("upload_type", "resumable")
-
-        const response = await fetch(`${FACEBOOK_API_GRAPH_URL}/${id}/media`, {
+        return fetch(`${FACEBOOK_API_GRAPH_URL}/${id}/media`, {
           method: "POST",
           body: formData,
           headers: {
             "Authorization": `OAuth ${access_token}`
           }
-        })
-
-        const container = await response.json()
-
-        if (container.id) {
-
-          const response = await fetch(container.uri, {
-            method: "POST",
-            headers: {
-              "Authorization": `OAuth ${access_token}`,
-              "file_url": url.source,
-            }
-          })
-
-          const upload = await response.json()
-
-          if (upload.success) {
-
-            const story = await publishContainerId(data, container.id)
-
-            if(story.id) {
-              return { ok: true }
-            } else {
-              console.log(story)
-              throw new Error(story.error.message)
-            }
-
-          } else {
-            console.log(upload)
-
-            if(JSON.parse(upload.debug_info.message)?.error.message === "Video process failed with error: Unsupported format: The video format is not supported. Please check spec for supported duration format") {
-              return { error: "The video must be shorter than 60 seconds. Upload another video" }
-            }
-
-            throw new Error(upload.debug_info.message)
-          }
-        } else throw new Error(container.error.message)
-
-      }
-
-    } else {
-
-      const mediaPromises = urls.map((item) => {
-
-        const formData = new FormData()
-
-        item.type === "image" ? formData.append("image_url", item.source) : formData.append("video_url", item.source)
-        formData.append("media_type", "STORIES")
-        formData.append("is_carousel_item", true)
-        formData.append("access_token", access_token)
-
-        return fetch(`${FACEBOOK_API_GRAPH_URL}/${id}/media`, {
-          method: "POST",
-          body: formData
         })
       })
 
@@ -124,14 +44,41 @@ export async function handler(data) {
 
       const media = await Promise.all(mediaData)
 
+      if (video) {
+
+        const uploadURI = media.find((container) => container.uri !== undefined).uri
+
+        const uploadVideo = await fetch(uploadURI, {
+          method: "POST",
+          headers: {
+            "Authorization": `OAuth ${access_token}`,
+            "file_url": video.source
+          }
+        })
+
+        const upload = await uploadVideo.json()
+
+        if (!upload.success) {
+
+          if (JSON.parse(upload.debug_info.message)?.error.message === "Video process failed with error: Unsupported format: The video format is not supported. Please check spec for supported frame_rate format") {
+            return { error: "The video dimensions are wrong. Edit your video or upload another." }
+          }
+
+          if (JSON.parse(upload.debug_info.message)?.error.message === "Video process failed with error: Unsupported format: The video format is not supported. Please check spec for supported duration format") {
+            return { error: "The video must be shorter than 60 seconds. Upload another video" }
+          }
+
+          throw new Error(upload.debug_info.message)
+        }
+      }
+
       const children = media.map((container) => container.id)
 
-      const childrenPromises = children.map((children) => {
-        publishContainerId(data, children)
-      })
+      const childrenPromises = children.map((id) => publishContainerId(data, id))
 
-      await Promise.all(childrenPromises)
-    }
+      const containersIds = await Promise.all(childrenPromises)
+
+      return { ok: true, data: containersIds }
 
   } catch (error) {
     console.log(error)
