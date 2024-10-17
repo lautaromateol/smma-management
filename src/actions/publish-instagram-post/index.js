d659710c48e2cbc2ebc8597278f72e43cfccbb69
lautaromateol <lautaromateol@gmail.com>
2024-10-17 01:07:26 -0300

    Acción formateada

--- src/actions/publish-instagram-post/index.js
+++ src/actions/publish-instagram-post/index.js
@@ -40,8 +40,8 @@ export async function handler(data) {
         if (response.ok) {
           creation_id = data.id
         } else {
-          console.log(data.error)
-          return { error: "Error uploading image!" }
+          console.log(data)
+          throw new Error(data.error.message)
         }
       } else {
         const videoUrl = urls[0].source
@@ -65,8 +65,8 @@ export async function handler(data) {
         if (response.ok) {
           creation_id = data.id
         } else {
-          console.log(data.error)
-          return { error: "Error uploading video!" }
+          console.log(data)
+          throw new Error(data.error.message)
         }
       }
 
@@ -77,15 +77,15 @@ export async function handler(data) {
 
       const mediaPromises = urls.map((item) => {
         const formData = new FormData()
-        formData.append("is_carousel_item", true)          
-        
-        if(item.type === "image") {
+        formData.append("is_carousel_item", true)
+
+        if (item.type === "image") {
           formData.append("image_url", item.source)
         } else {
           formData.append("media_type", "VIDEO")
           formData.append("upload_type", "resumable")
         }
-        
+
         return fetch(`${FACEBOOK_API_GRAPH_URL}/${id}/media`, {
           method: "POST",
           headers: {
@@ -101,33 +101,33 @@ export async function handler(data) {
 
       const media = await Promise.all(mediaData)
 
-      if(video) {
-        
-      const uploadURI = media.find((container) => container.uri !== undefined).uri
+      if (video) {
 
-      const uploadVideo = await fetch(uploadURI, {
-        method: "POST",
-        headers: {
-          "Authorization": `OAuth ${access_token}`,
-          "file_url": video.source
+        const uploadURI = media.find((container) => container.uri !== undefined).uri
+
+        const uploadVideo = await fetch(uploadURI, {
+          method: "POST",
+          headers: {
+            "Authorization": `OAuth ${access_token}`,
+            "file_url": video.source
+          }
+        })
+
+        const upload = await uploadVideo.json()
+
+        if (!upload.success) {
+
+          if (JSON.parse(upload.debug_info.message)?.error.message === "Video process failed with error: Unsupported format: The video format is not supported. Please check spec for supported frame_rate format") {
+            return { error: "The video dimensions are wrong. Edit your video or upload another." }
+          }
+
+          if (JSON.parse(upload.debug_info.message)?.error.message === "Video process failed with error: Unsupported format: The video format is not supported. Please check spec for supported duration format") {
+            return { error: "The video must be shorter than 60 seconds. Upload another video" }
+          }
+
+          throw new Error(upload.debug_info.message)
         }
-      })
-      
-      const upload = await uploadVideo.json()
-
-      if(!upload.success) {
-
-        if(JSON.parse(upload.debug_info.message)?.error.message === "Video process failed with error: Unsupported format: The video format is not supported. Please check spec for supported frame_rate format") {
-          return { error: "The video dimensions are wrong. Edit your video or upload another." }
-        }
-
-        if(JSON.parse(upload.debug_info.message)?.error.message === "Video process failed with error: Unsupported format: The video format is not supported. Please check spec for supported duration format") {
-          return { error: "The video must be shorter than 60 seconds. Upload another video" }
-        }
-        
-        throw new Error(upload.debug_info.message)
       }
-    }
 
       const children = media.map((container) => container.id)
 
@@ -149,8 +149,8 @@ export async function handler(data) {
       if (response.ok) {
         creation_id = data.id
       } else {
-        console.log(data.error)
-        return { error: "Error uploading post to Instagram!" }
+        console.log(data)
+        throw new Error(data.error.message)
       }
     }
 
@@ -159,8 +159,8 @@ export async function handler(data) {
     if (upload.id) {
       return { ok: true, id: upload.id }
     } else {
-      console.log(upload.error)
-      return { error: "Error uploading the post to Instagram!" }
+      console.log(upload)
+      throw new Error(upload.error.message)
     }
   } catch (error) {
     console.log(error)
