889350e54d2d30484b23f79330f71bbcfd69f39c
lautaromateol <lautaromateol@gmail.com>
2024-10-18 19:56:28 -0300

    Campos modificados para actualización de post

--- src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/meta-posts.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/meta-posts.jsx
@@ -2,17 +2,16 @@ import { FACEBOOK_API_GRAPH_URL } from "@/constants/facebook"
 import { fetcher } from "@/lib/fetcher"
 import { FaFacebook, FaInstagram } from "react-icons/fa6"
 import { MetaPostsTable } from "."
-import { addMilliseconds } from "date-fns"
 
 export async function MetaPosts({ data }) {
 
-  const { fbPageName, fbPageId, fbPictureUrl, igPageName, igPageId, igPictureUrl, pageAccessToken } = data
+  const { fbPageName, fbPageId, fbPictureUrl, igPageName, igPageId, igPictureUrl, pageAccessToken, userAccessToken } = data
 
   const fbData = await fetcher(`${FACEBOOK_API_GRAPH_URL}/${fbPageId}/feed?fields=id,created_time,message,attachments{media_type,media,url}`, {
     "Authorization": `OAuth ${pageAccessToken}`
   })
 
-  const fbScheduledData = await fetcher(`${FACEBOOK_API_GRAPH_URL}/${fbPageId}/scheduled_posts?fields=id,scheduled_publish_time,message,attachments{media_type,media,url}`,{
+  const fbScheduledData = await fetcher(`${FACEBOOK_API_GRAPH_URL}/${fbPageId}/scheduled_posts?fields=id,scheduled_publish_time,message,attachments{media_type,media,url}`, {
     "Authorization": `OAuth ${pageAccessToken}`
   })
 
@@ -29,40 +28,62 @@ export async function MetaPosts({ data }) {
   const fbPosts = fbUnformatted.map((fbPost) => {
 
     const post = {
+      id: fbPost.id,
       message: fbPost.message ?? "",
       page: fbPageName,
       picture: fbPictureUrl,
       media_url: fbPost.attachments?.data[0]?.media?.image?.src ?? "",
+      media: fbPost.attachments ? fbPost.attachments.data.map(({ media_type, media, url }) => {
+        return {
+          media_type: media_type === "album" || media_type === "photo" ? "IMAGE" : "LINK",
+          media_url: media_type === "album" || media_type === "photo" ? media.image.src : url
+        }
+      }) : [],
       icon: <FaFacebook className="text-blue-500" />,
+      platform: "FACEBOOK",
+      published: true
     }
 
-    return { post, created_time: fbPost.created_time, url: fbPost.attachments?.data[0]?.url ?? "", caption: fbPost.message }
+    return { post, created_time: fbPost.created_time, url: fbPost.attachments?.data[0]?.url ?? "", caption: fbPost.message, data }
   })
 
   const fbScheduled = fbScheduledUnformatted.map((fbPost) => {
 
     const post = {
+      id: fbPost.id,
       message: fbPost.message ?? "",
       page: fbPageName,
       picture: fbPictureUrl,
       media_url: fbPost.attachments?.data[0]?.media?.image?.src ?? "",
+      media: fbPost.attachments ? fbPost.attachments.data.map(({ media_type, media, url }) => {
+        return {
+          media_type: media_type === "album" || media_type === "photo" ? "IMAGE" : "LINK",
+          media_url: media_type === "album" || media_type === "photo" ? media.image.src : url
+        }
+      }) : [],
       icon: <FaFacebook className="text-blue-500" />,
+      platform: "FACEBOOK",
+      published: false,
     }
 
-    return { post, created_time: new Date(fbPost.scheduled_publish_time), url: fbPost.attachments?.data[0]?.url ?? "", caption: fbPost.message }
+    return { post, created_time: new Date(fbPost.scheduled_publish_time * 1000).toISOString(), url: fbPost.attachments?.data[0]?.url ?? "", caption: fbPost.message, data }
   })
 
   const igPosts = igUnformatted.map((igPost) => {
-    
+
     const post = {
+      id: igPost.id,
       message: igPost.caption,
       page: igPageName,
       picture: igPictureUrl,
       media_url: igPost.children ? igPost.media_url : igPost.thumbnail_url,
-      icon: <FaInstagram className="text-pink-600" />
+      media: igPost.children ? igPost.children.data : [{ media_type: igPost.thumbnail_url ? "VIDEO" : "IMAGE", media_url: igPost.media_url }],
+      icon: <FaInstagram className="text-pink-600" />,
+      platform: "INSTAGRAM",
+      published: true
     }
 
-    return { post, created_time: igPost.timestamp, caption: igPost.caption }
+    return { post, created_time: igPost.timestamp, caption: igPost.caption, data }
   })
 
   const posts = [...fbPosts, ...igPosts].sort(
