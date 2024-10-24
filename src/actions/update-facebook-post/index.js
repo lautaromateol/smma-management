6ce0dba21510ffb8bba7a894d4cfc426334c000f
lautaromateol <lautaromateol@gmail.com>
2024-10-24 19:14:03 -0300

    Acción y esquema para editar posteo de FB

---
+++ src/actions/update-facebook-post/index.js
@@ -0,0 +1,51 @@
+"use server"
+import { auth } from "@clerk/nextjs/server";
+import { FacebookPostToUpdate } from "./schema";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { FACEBOOK_API_GRAPH_URL } from "@/constants/facebook";
+import { revalidatePath } from "next/cache";
+import { parseISO } from "date-fns";
+
+export async function handler(data) {
+  const { userId, orgId } = auth()
+
+  if (!userId || !orgId) return {
+    error: "Unauthorized"
+  }
+
+  const { id, post_id, access_token, scheduled_publish_time, ...rest } = data
+
+  try {
+
+    const date = new Date(scheduled_publish_time)
+
+    const timestamp = Math.floor(date.getTime() / 1000)
+
+    const response = await fetch(`${FACEBOOK_API_GRAPH_URL}/${post_id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...rest,
+        scheduled_publish_time: timestamp,
+        access_token
+      })
+    })
+
+    const data = await response.json()
+
+    if (response.ok) {
+      revalidatePath(`/agency/${orgId}/campaigns/${userId}`)
+      return { ok: true, id: data.id }
+    } else {
+      console.log(data)
+      throw new Error(data.error.message)
+    }
+  } catch (error) {
+    console.log(error)
+    return { error: "Error updating the Facebook post!" }
+  }
+}
+
+export const updateFacebookPost = createSafeAction(FacebookPostToUpdate, handler)
---
+++ src/actions/update-facebook-post/schema.js
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const FacebookPostToUpdate = z.object({
+  id: z.string({
+    message: "Facebook page ID is required"
+  }),
+  post_id: z.string({
+    message: "Facebook post ID is required"
+  }),
+  message: z.string({
+    message: "Caption text cannot be empty"
+  }).min(1, {
+    message: "Caption text cannot be empty"
+  }).optional(),
+  location: z.string().optional().nullable(),
+  scheduled_publish_time: z.date().optional().nullable(),
+  unpublished_content_type: z.string().optional(),
+  access_token: z.string({
+    message: "Please provide a page access token"
+  })
+})
