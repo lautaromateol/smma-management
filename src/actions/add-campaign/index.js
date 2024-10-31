92550e070c8d5c2500a986a80cc78f1eeb3433f9
lautaromateol <lautaromateol@gmail.com>
2024-10-31 18:57:21 -0300

    Acción para crear campañas actualizada

--- src/actions/add-campaign/index.js
+++ src/actions/add-campaign/index.js
@@ -6,6 +6,7 @@ import { createSafeAction } from "@/lib/create-safe-action";
 import { revalidatePath } from "next/cache";
 import { Action, EntityType } from "@prisma/client";
 import { createAudtiLog } from "@/lib/create-audit-log";
+import { FACEBOOK_API_GRAPH_URL } from "@/constants/facebook";
 
 export async function handler(data) {
   const { userId, orgId } = auth()
@@ -14,41 +15,78 @@ export async function handler(data) {
     error: "Unauthorized"
   }
 
-  const { name, client, budget, start, end, objective, platforms } = data
+  const { name, client, budget, start, end, objective, platform } = data
 
   const dbClient = await prisma.client.findUnique({
     where: { id: client }
   })
 
-  if(!dbClient) {
+  if (!dbClient) {
     return { error: "This client is not registered" }
   }
 
   try {
 
-    const campaign = await prisma.campaign.create({
-      data: {
+    const [metaAccessToken, metaAdAccountId] = await Promise.all([
+      prisma.metaAccessToken.findFirst({
+        where: {
+          clientId: dbClient.id
+        }
+      }),
+      prisma.metaAdAccountId.findFirst({
+        where: {
+          clientId: dbClient.id
+        }
+      })
+    ])
+
+    const response = await fetch(`${FACEBOOK_API_GRAPH_URL}/${metaAdAccountId.id}/campaigns`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${metaAccessToken.token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
         name,
-        clientId: client,
-        budget: Number(budget),
-        start: new Date(start),
-        end: new Date(end),
-        platforms,
         objective,
-        orgId
-      }
-    })
+        status: "ACTIVE",
+        special_ad_categories: [],
+        start_time: new Date(start),
+        stop_time: new Date(end)
+      })
+    });
 
-    await createAudtiLog({
-      action: Action.CREATE,
-      entityType: EntityType.CAMPAIGN,
-      entityTitle: campaign.name,
-      entityId: campaign.id
-    })
+    const campaignData = await response.json();
 
-    revalidatePath(`/agency/${orgId}/campaigns`)
+    if (campaignData.id) {
+      const campaign = await prisma.campaign.create({
+        data: {
+          id: campaignData.id,
+          name,
+          clientId: dbClient.id,
+          budget: Number(budget),
+          start: new Date(start),
+          end: new Date(end),
+          platform,
+          objective,
+          orgId,  
+        }
+      })
 
-    return { ok: true, data: campaign }
+      await createAudtiLog({
+        action: Action.CREATE,
+        entityType: EntityType.CAMPAIGN,
+        entityTitle: campaign.name,
+        entityId: campaign.id
+      })
+
+      revalidatePath(`/agency/${orgId}/campaigns`)
+
+      return { ok: true, data: campaign }
+    } else {
+      throw new Error(campaignData.error.message)
+    }
+
   } catch (error) {
     console.log(error)
     return { error: "Error creating the campaign!" }
