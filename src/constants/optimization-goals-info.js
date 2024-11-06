c77f028e609e38d28c9d458e75152aac5f34c97a
lautaromateol <lautaromateol@gmail.com>
2024-11-05 22:57:47 -0300

    Array de objetivos de optimización v1

---
+++ src/constants/optimization-goals-info.js
@@ -0,0 +1,12 @@
+export const optimizationGoalsInfo = [
+  {
+    goal: "REACH",
+    title: "Maximise reach of ads",
+    description: "We'll try to show your ads to as many people as possible."
+  }, 
+  {
+    goal: "IMPRESSIONS",
+    title: "Maximise number of impressions",
+    description: "We'll try to show your ads to people as many times as possible."
+  }
+]
