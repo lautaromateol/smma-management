c968ebbaac1fbea351f0f397a7630518aa2ac334
lautaromateol <lautaromateol@gmail.com>
2024-11-05 23:00:30 -0300

    Formulario de Ad Set v0

--- src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/meta-ad-form.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/meta-ad-form.jsx
@@ -1,103 +1,68 @@
 "use client"
-import { cloneElement, useState } from "react"
-import { MessageSquare, FlaskConical, Facebook, Instagram, Megaphone, Phone, MapPin, MousePointerClick, Globe, IdCard } from 'lucide-react';
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AdSet } from "@/actions/publish-ad-set/schema";
+import { AdTargeting, Locations, OptimizationGoal, ScheduleAdSet } from "./ads";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-const goals = [
-    {
-        goal: "Get More Messages",
-        description: null,
-        icon: <MessageSquare />
-    },
-    {
-        goal: "Create A/B Test",
-        description: null,
-        icon: <FlaskConical />
-    },
-    {
-        goal: "Boost Facebook Content",
-        description: "Get more people to see and engage with your Page posts",
-        icon: <Facebook />
-    },
-    {
-        goal: "Boost an Instagram Post or Reel",
-        description: null,
-        icon: <Instagram />
-    },
-    {
-        goal: "Promote Your Page",
-        description: null,
-        icon: <Megaphone />
-    },
-    {
-        goal: "Get More Calls",
-        description: null,
-        icon: <Phone />
-    },
-    {
-        goal: "Promote Your Business Locally",
-        description: "Connect with people nearby",
-        icon: <MapPin />
-    },
-    {
-        goal: "Get More Button Clicks",
-        description: "Create an ad that includes the call-to-action button from your Page",
-        icon: <MousePointerClick />
-    },
-    {
-        goal: "Get More Website Visitors",
-        description: "Create an ad to send people to your website",
-        icon: <Globe />
-    },
-    {
-        goal: "Get more Leads",
-        description: "Create an ad to request contact details from potential customers",
-        icon: <IdCard />
+export function MetaAdForm({ data }) {
+
+    const { campaign } = data
+
+    const form = useForm({
+        resolver: zodResolver(AdSet),
+        defaultValues: {
+            status: "ACTIVE",
+            campaign_id: campaign.id
+        }
+    })
+
+    const { errors } = form.formState
+
+    console.log(errors)
+
+    function onSubmit(data) {
+        console.log(data)
     }
-];
 
-
-export function MetaAdForm() {
-
-    const [step, setStep] = useState(1)
-
-    if (step === 1) {
-        return (
-            <div className="space-y-4">
-                <h2 className="text-lg font-semibold">Choose a goal</h2>
-                <div className="grid grid-cols-4 gap-2">
-                    {goals.map(({ goal, description, icon }, i) => (
-                        <AddGoalCard
-                            key={i}
-                            goal={goal}
-                            description={description}
-                            icon={icon}
+    return (
+        <div className="bg-main-light p-8 max-w-5xl">
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col space-y-4">
+                    <div className="bg-white p-4">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Ad Set Name</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
                         />
-                    ))}
-                </div>
-            </div>
-        )
-    }
-
-    return (
-        <div></div>
-    )
-}
-
-function AddGoalCard({ goal, description, icon }) {
-    return (
-        <div
-            role="button"
-            className="flex flex-col space-y-2 py-6 px-4 shadow rounded-md"
-        >
-            <div className="grid place-content-center size-12 p-4 rounded-full bg-muted">
-                {cloneElement(icon, { className: "size-8" })}
-            </div>
-            <p className="text-base font-semibold">
-                {goal}
-            </p>
-            {description && <p className="text-sm font-light">
-                {description}
-            </p>}
+                    </div>
+                    <OptimizationGoal data={data} form={form} />
+                    <ScheduleAdSet form={form} />
+                    <div className="bg-white p-4 space-y-4">
+                        <div className="flex flex-col space-y-2">
+                            <FormLabel>Audience controls</FormLabel>
+                            <FormDescription>Set criteria for where ads for this campaign can be delivered.</FormDescription>
+                        </div>
+                        <Locations data={data} form={form} />
+                    </div>
+                    <Button
+                        type="submit"
+                        variant="main"
+                    >
+                        Submit
+                    </Button>
+                </form>
+            </Form>
         </div>
     )
 }
