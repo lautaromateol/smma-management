a0c5bb8cb11d141fa5b5953a8cc3c5b6e4b773d1
lautaromateol <lautaromateol@gmail.com>
2024-11-08 18:20:34 -0300

    Array de Ad Sets añadidos a tabla

--- src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/meta-content.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/meta-content.jsx
@@ -3,10 +3,10 @@ import { getMetaContent } from "@/lib/get-meta-content"
 
 export async function MetaContent({ data }) {
 
-  const { posts, scheduledPosts } = await getMetaContent(data)
+  const { posts, scheduledPosts, adSets } = await getMetaContent(data)
 
   return (
-    <MetaPostsTable posts={posts} scheduledPosts={scheduledPosts} />
+    <MetaPostsTable posts={posts} scheduledPosts={scheduledPosts} adSets={adSets} />
   )
 }
 
