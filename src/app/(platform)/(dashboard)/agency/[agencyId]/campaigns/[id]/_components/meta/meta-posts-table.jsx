eaa49733cb929a99ea98a11e4efc83afa2b5c6a3
lautaromateol <lautaromateol@gmail.com>
2024-11-08 18:19:34 -0300

    Tabla de Adsets añadida a sección de contenido

--- src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/meta-posts-table.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/meta/meta-posts-table.jsx
@@ -1,14 +1,19 @@
 "use client"
 import { useState } from "react"
+import { cn } from "@/lib/utils"
 import { CardWrapper } from "@/components/card-wrapper"
 import { DataTable } from "@/components/data-table"
-import { Separator } from "@radix-ui/react-dropdown-menu"
-import { metaPostsColumns } from "./meta-posts-columns"
-import { cn } from "@/lib/utils"
+import { Select, SelectGroup, SelectItem, SelectTrigger, SelectContent, SelectValue } from "@/components/ui/select"
+import { Separator } from "@/components/ui/separator"
+import { adSetsColumns, metaPostsColumns } from "./meta-content-columns"
 
-export function MetaPostsTable({ posts, scheduledPosts }) {
+export function MetaPostsTable({ posts, scheduledPosts, adSets }) {
 
   const [showScheduled, setShowScheduled] = useState(false)
+
+  const [showAds, setShowAds] = useState(false)
+
+  const [displayPosts, setDisplayPosts] = useState(true)
 
   return (
     <CardWrapper
@@ -16,35 +21,86 @@ export function MetaPostsTable({ posts, scheduledPosts }) {
       description="Review your published and scheduled posts in this campaign"
     >
       <div className="space-y-4">
-        <div className="flex items-center gap-x-2">
-          <div
-            onClick={() => setShowScheduled(false)}
-            role="button"
-            className={cn(
-              "p-2 rounded-md text-sm",
-              !showScheduled && "bg-main-shade text-main-tint"
-            )}
-          >
-            Published
-          </div>
-          <div
-            onClick={() => setShowScheduled(true)}
-            role="button"
-            className={cn(
-              "p-2 rounded-md text-sm",
-              showScheduled && "bg-main-shade text-main-tint"
-            )}
-          >
-            Scheduled
+        <div className="flex items-center justify-between">
+          {displayPosts ?
+            <div className="flex items-center gap-x-2">
+              <div
+                onClick={() => setShowScheduled(false)}
+                role="button"
+                className={cn(
+                  "p-2 rounded-md text-sm",
+                  !showScheduled && "bg-main-shade text-main-tint"
+                )}
+              >
+                Published
+              </div>
+              <div
+                onClick={() => setShowScheduled(true)}
+                role="button"
+                className={cn(
+                  "p-2 rounded-md text-sm",
+                  showScheduled && "bg-main-shade text-main-tint"
+                )}
+              >
+                Scheduled
+              </div>
+            </div>
+            :
+            <div className="flex items-center gap-x-2">
+              <div
+                onClick={() => setShowAds(false)}
+                role="button"
+                className={cn(
+                  "p-2 rounded-md text-sm",
+                  !showAds && "bg-main-shade text-main-tint"
+                )}
+              >
+                Ad Sets
+              </div>
+              <div
+                onClick={() => setShowAds(true)}
+                role="button"
+                className={cn(
+                  "p-2 rounded-md text-sm",
+                  showAds && "bg-main-shade text-main-tint"
+                )}
+              >
+                Ads
+              </div>
+            </div>}
+          <div className="w-auto">
+            <Select onValueChange={(value) => setDisplayPosts(value)}>
+              <SelectTrigger>
+                <SelectValue>
+                  <p className="font-medium">
+                    {displayPosts ? "Posts" : "Ads"}
+                  </p>
+                </SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value={true}>Posts</SelectItem>
+                  <SelectItem value={false}>Ads</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
           </div>
         </div>
         <Separator className="w-full" />
-        <DataTable
-          data={showScheduled ? scheduledPosts : posts}
-          columns={metaPostsColumns}
-          filterBy="caption"
-        />
+        {displayPosts ?
+          <DataTable
+            data={showScheduled ? scheduledPosts : posts}
+            columns={metaPostsColumns}
+            filterBy="caption"
+          />
+          :
+          <DataTable
+            data={adSets}
+            columns={adSetsColumns}
+            filterBy="name"
+          />
+        }
       </div>
-    </CardWrapper>
+    </CardWrapper >
   )
 }
