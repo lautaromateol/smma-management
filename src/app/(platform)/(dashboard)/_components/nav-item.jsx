36a74a6ec169387b88a4e4f13389b47169178c72
lautaromateol <lautaromateol@gmail.com>
2024-11-12 15:31:50 -0300

    Botones de navegación re-estilizados

--- src/app/(platform)/(dashboard)/_components/nav-item.jsx
+++ src/app/(platform)/(dashboard)/_components/nav-item.jsx
@@ -2,25 +2,48 @@
 import Link from "next/link"
 import { cn } from "@/lib/utils"
 import { usePathname } from "next/navigation"
-import { cloneElement } from "react"
+import { cloneElement, useState } from "react"
 
-export function NavItem({ title, href, icon }) {
+export function NavItem({ title, href, icon, onClick }) {
+
+  const [onHover, setOnHover] = useState(false)
 
   const pathname = usePathname()
   const location = pathname.split("/")[3] || "/"
-  const hrefSplitted = href.split("/")[3] || "/"
+  const hrefSplitted = href?.split("/")[3] || "/"
 
-  return (
-    <Link
-      href={href}
-    >
-      <div className={cn(
-        "flex items-center px-4 py-2 gap-x-4 w-full rounded-md text-sm font-medium text-neutral-500 hover:shadow-sm hover:bg-main-shade hover:text-main-tint",
-        location === hrefSplitted ? "text-main-tint bg-main-shade" : ""
-      )}>
-        {cloneElement(icon, { className: "size-5" })}
+  if (!onClick) {
+    return (
+      <Link
+        href={href}
+      >
+        <div
+          onMouseEnter={() => setOnHover(true)}
+          onMouseLeave={() => setOnHover(false)}
+          className={cn(
+            "flex items-center px-2 py-1 gap-x-4 w-full text-sm font-light text-gray-700 hover:bg-main-shade",
+            location === hrefSplitted && "bg-main-shade",
+            onHover && "text-main-tint"
+          )}>
+          {cloneElement(icon, { className: "size-5 text-main font-thin" })}
+          {title}
+        </div>
+      </Link>
+    )
+  } else {
+    return (
+      <div
+        onClick={onClick}
+        onMouseEnter={() => setOnHover(true)}
+        onMouseLeave={() => setOnHover(false)}
+        role="button"
+        className={cn(
+          "flex items-center px-2 py-1 gap-x-4 w-full text-sm font-light text-gray-700 hover:bg-main-shade",
+          onHover && "text-main-tint"
+        )}>
+        {cloneElement(icon, { className: "size-5 text-main font-thin" })}
         {title}
       </div>
-    </Link>
-  )
+    )
+  }
 }
--- src/app/(platform)/(dashboard)/_components/sign-out-button.jsx
+++ src/app/(platform)/(dashboard)/_components/sign-out-button.jsx
@@ -1,18 +1,17 @@
 "use client"
 import { useClerk } from "@clerk/nextjs";
 import { ArrowUpRightFromSquare } from "lucide-react";
+import { NavItem } from "./nav-item";
 
 export function SignOutButton() {
 
   const { signOut } = useClerk()
 
   return (
-    <div
+    <NavItem
+      icon={<ArrowUpRightFromSquare />}
       onClick={() => signOut({ redirectUrl: "/" })}
-      role="button"
-      className="flex items-center px-4 py-2 gap-x-4 w-full rounded-md text-sm font-medium text-neutral-500 hover:shadow-sm hover:bg-main-shade hover:text-main-tint">
-      <ArrowUpRightFromSquare className="size-5" />
-      Sign Out
-    </div>
+      title="Sign Out"
+    />
   )
 }
