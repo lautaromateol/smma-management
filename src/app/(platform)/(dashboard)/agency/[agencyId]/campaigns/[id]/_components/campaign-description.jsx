3a094727cc0aa1dc38abe9ac40ba76377e06d722
lautaromateol <lautaromateol@gmail.com>
2024-11-12 15:30:40 -0300

    Información de campaña obsoleta eliminada

--- src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/campaign-description.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/campaign-description.jsx
@@ -1,16 +1,6 @@
-import { formatDate } from "@/lib/utils";
-
 export function CampaignDescription({ campaign }) {
   return (
     <div className="flex items-center gap-x-6">
-      <div className="flex items-center gap-x-2">
-        <p className="text-sm font-light">Start date:</p>
-        <p className="text-sm">{formatDate(new Date(campaign.start))}</p>
-      </div>
-      <div className="flex items-center gap-x-2">
-        <p className="text-sm font-light">End date:</p>
-        <p className="text-sm">{formatDate(new Date(campaign.end))}</p>
-      </div>
       <div className="flex items-center gap-x-2">
         <p className="text-sm font-light">Budget:</p>
         <p className="text-sm">{campaign.budget.toLocaleString("en-US", { style: "currency", currency: "USD" })}</p>
