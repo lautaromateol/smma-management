da21f313285bef83069ce683d38d4f215fe90056
lautaromateol <lautaromateol@gmail.com>
2024-11-12 15:29:06 -0300

    Header re-estilizado

--- src/app/(platform)/(dashboard)/_components/header.jsx
+++ src/app/(platform)/(dashboard)/_components/header.jsx
@@ -1,11 +1,11 @@
 import { OrganizationSwitcher } from "@clerk/nextjs"
 import { experimental__simple } from "@clerk/themes"
-import { Logo } from "../../../../components/logo"
+import { HandleSidebarBtn } from "./hadle-sidebar-button"
 
 export function Header() {
   return (
-    <header className="h-14 z-50 sticky top-0 shadow p-4 flex items-center justify-between border-b bg-main-light border-main-shade">
-      <Logo />
+    <header className="h-14 z-50 sticky top-0 p-4 flex items-center justify-between border-b bg-white border-gray-100">
+      <HandleSidebarBtn />
       <OrganizationSwitcher
         appearance={{
           baseTheme: experimental__simple
