b0b39f78fb305b5a224c97a2193debfe978fa0ca
lautaromateol <lautaromateol@gmail.com>
2024-11-12 15:31:29 -0300

    Fuente de aplicación actualizada

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Montserrat } from "next/font/google";
+import { Inter } from "next/font/google"
 import "./globals.css";
 
-const montserrat = Montserrat({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], weight: ["100" ,"200", "300", "400", "500", "600", "700", "900"] })
 
 export const metadata = {
   title: {
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
