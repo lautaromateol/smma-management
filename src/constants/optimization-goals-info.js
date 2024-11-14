5f31cadfb7dceb9cb13ba37bc1864f7b85a114db
lautaromateol <lautaromateol@gmail.com>
2024-11-13 22:35:37 -0300

    Información de objetivos de optimización añadida

--- src/constants/optimization-goals-info.js
+++ src/constants/optimization-goals-info.js
@@ -8,5 +8,24 @@ export const optimizationGoalsInfo = [
     goal: "IMPRESSIONS",
     title: "Maximise number of impressions",
     description: "We'll try to show your ads to people as many times as possible."
+  },
+  {
+    goal: "LANDING_PAGE_VIEWS",
+    title: "Maximise number of landing page views",
+    description: "We'll try to show your ads to the people most likely to view the website or Instant Experience linked in your ad."
+  },
+  {
+    goal: "LINK_CLICKS",
+    title: "Maximise number of link clicks",
+    description: "We'll try to show your ads to the people most likely to click on them."
+  },
+  {
+    goal: "CONVERSATIONS",
+    title: "Maximise number of conversations",
+    description: "We'll try to show your ads to people most likely to have a conversation with you through messaging."
+  },
+  {
+    goal: "VISIT_INSTAGRAM_PROFILE",
+    title: "Maximise number of Instagram profile visits"
   }
 ]
