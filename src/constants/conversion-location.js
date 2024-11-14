58375e5649588f51e9e1ee17b920d788ca50fb5d
lautaromateol <lautaromateol@gmail.com>
2024-11-13 22:36:09 -0300

    Objetivos de trafico añadidos

---
+++ src/constants/conversion-location.js
@@ -0,0 +1,24 @@
+export const conversionLocation = [
+  // {
+  //   title: "Website",
+  //   description: "Send traffic to your website.",
+  //   related_goals: ["LANDING_PAGE_VIEWS", "LINK_CLICKS"],
+  //   secondary_goals: ["IMPRESSIONS", "CONVERSATIONS"]
+  // },
+  // {
+  //   title: "App",
+  //   description: "Send traffic to your app.",
+  //   related_goals: ["LINK_CLICKS"]
+  // },
+  {
+    title: "Messaging apps",
+    description: "Send traffic to Instagram, Messenger and Whatsapp.",
+    related_goals: ["LINK_CLICKS", "CONVERSATIONS"],
+    secondary_goals: ["IMPRESSIONS"]
+  },
+  {
+    title: "Instagram profile",
+    description: "Send traffic to your Instagram profile",
+    related_goals: ["VISIT_INSTAGRAM_PROFILE"]
+  }
+]
