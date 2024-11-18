f254dc24d03f839382e3563fa1fdc9f2bc18a5f6
lautaromateol <lautaromateol@gmail.com>
2024-11-18 20:30:35 -0300

    Tipo de presupuesto añadido a descripción

--- src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/campaign-description.jsx
+++ src/app/(platform)/(dashboard)/agency/[agencyId]/campaigns/[id]/_components/campaign-description.jsx
@@ -2,8 +2,8 @@ export function CampaignDescription({ campaign }) {
   return (
     <div className="flex flex-col md:flex-row items-start md:items-center gap-x-6">
       <div className="flex items-center gap-x-2">
-        <p className="text-sm font-light">Budget:</p>
-        {/* <p className="text-sm">{campaign.budget.toLocaleString("en-US", { style: "currency", currency: "USD" })}</p> */}
+        <p className="text-sm font-light">{campaign.daily_budget ? "Daily budget" : "Lifetime budget"}:</p>
+        <p className="text-sm">{campaign[campaign.daily_budget ? "daily_budget" : "lifetime_budget"].toLocaleString("en-US", { style: "currency", currency: "USD" })}</p>
       </div>
       <div className="flex items-center gap-x-2">
         <p className="text-sm font-light">Objective:</p>
