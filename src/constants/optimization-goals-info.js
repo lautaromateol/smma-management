7162f5df5a96d54aa5576e64884cca2c19ea3633
lautaromateol <lautaromateol@gmail.com>
2024-11-18 20:29:05 -0300

    Objetivos de optimización añadidos

--- src/constants/optimization-goals-info.js
+++ src/constants/optimization-goals-info.js
@@ -3,7 +3,7 @@ export const optimizationGoalsInfo = [
     goal: "REACH",
     title: "Maximise reach of ads",
     description: "We'll try to show your ads to as many people as possible."
-  }, 
+  },
   {
     goal: "IMPRESSIONS",
     title: "Maximise number of impressions",
@@ -26,6 +26,47 @@ export const optimizationGoalsInfo = [
   },
   {
     goal: "VISIT_INSTAGRAM_PROFILE",
-    title: "Maximise number of Instagram profile visits"
+    title: "Maximise number of Instagram profile visits",
+    description: "We'll try to deliver your ads to the right people to help you get more visits at your Instagram profile."
+  },
+  {
+    goal: "QUALITY_CALL",
+    title: "Maximise number of calls",
+    description: "We'll try to deliver your ads to try to get you the most possible calls and report the number of times that the call button in the call confirmation dialogue is clicked."
+  },
+  {
+    goal: "PAGE_LIKES",
+    title: "Maximise number of Page likes",
+    description: "We'll try to deliver your ads to the right people to help you get more Page likes at the lowest cost."
+  },
+  {
+    goal: "AD_RECALL_LIFT",
+    title: "Maximise ad recall lift",
+    description: "We'll try to show your ads to people who are likely to remember seeing them."
+  },
+  {
+    goal: "OFFSITE_CONVERSIONS",
+    title: "Maximise number of conversions",
+    description: "We'll try to show your ads to the people most likely to take a specific action on your website."
+  },
+  {
+    goal: "APP_INSTALLS_AND_OFFSITE_CONVERSIONS",
+    title: "Maximise number of app events",
+    description: "We'll try to show your ads to the people most likely to take a specific action in your app at least once."
+  },
+  {
+    goal: "LEAD_GENERATION",
+    title: "Maximise number of leads",
+    description: "We'll try to show your ads to the people most likely to share their contact information with you"
+  },
+  {
+    goal: "QUALITY_LEAD",
+    title: "Maximise number of conversion leads",
+    description: "We'll try to show your ads to the people most likely to convert after sharing their contact information with you."
+  },
+  {
+    goal: "APP_INSTALLS",
+    title: "Maximise number of app installs",
+    description: "We'll try to show your ads to the people most likely to install your app."
   }
 ]
